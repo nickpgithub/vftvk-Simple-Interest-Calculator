a269b7981364b8f703ba7ab2ea326c7eb2ab7e17
Nick Patel <iamnickpatel@gmail.com>
2022-01-11 18:27:31 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){
+    var principal = document.getElementById("principal").value;
+    var moreThenZero = parseInt(principal) > 0;
+    if (!moreThenZero) {
+    alert("Enter a positive number in the Amount Field Please");
+    document.getElementById("principal").focus();
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    result.innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    }
     
-}
-        
+    function updateRate()
+    {
+    var rateval = document.getElementById("rate").value+"%";
+    document.getElementById("rate_val").innerText=rateval;
+    }
+    
