064018db815e0585e5b2ed758df015d7da465b6d
Nick Patel <iamnickpatel@gmail.com>
2022-01-11 19:21:01 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function compute(){
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    result.innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    result.innerHTML = "If you deposit \<mark\>"+principal+"</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\>You will receive an amount of \<mark\>"+interest+",\</mark\>\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>";
     }
     
     function updateRate()
